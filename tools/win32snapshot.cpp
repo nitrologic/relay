4b2f350fab5942ff9fea6cdb52c74ac452dd6760
simon ryzen5 <nitrologic@gmail.com>
2026-07-02 16:53:55 +1200

    snapshot 6

--- tools/win32snapshot.cpp
+++ tools/win32snapshot.cpp
@@ -9,24 +9,39 @@
 #include "base64.h"
 
 extern "C" bool writeJPG(const char* filename, const void* pPixels, int width, int height, int quality);
-extern "C" bool writePNG(const char* filename, const void* pPixels, int width, int height);
+extern "C" bool writePNG(const char* filename, const void* pPixels, int width, int height, unsigned char *buffer);
 extern "C" bool writeBMP(const char* filename, const void* pPixels, int width, int height);
 
-int main(int argc, char* argv[]) {
-	SetProcessDPIAware();
+const char* bmp_dest = "desktop.bmp";
+const char* jpg_dest = "desktop.jpg";
 
-	const char* dest = (argc > 1) ? argv[1] : "desktop.png";
-	const char* bmp_dest = "desktop.bmp";
-	const char* jpg_dest = "desktop.jpg";
+unsigned char lineBuffer[16384];
 
-	int width = GetSystemMetrics(SM_CXSCREEN);
-	int height = GetSystemMetrics(SM_CYSCREEN);
+bool writeImageFile(const char *png_dest,void *pixels,int width,int height){
+	// write bmp file
+	writeBMP(bmp_dest,pixels,width,height);
+	std::cout << "snapshot saved bmp " << bmp_dest << ": [" << width << "," << height << "]" <<  std::endl;
+	// write jpg file
+	bool ok1=writeJPG(jpg_dest,pixels,width,height,72);
+	if(ok1){
+		std::cout << "snapshot saved jpg " << jpg_dest << ": [" << width << "," << height << "]" <<  std::endl;
+	}else{
+		std::cout << "snapshot failed to save jpg " << jpg_dest << ": [" << width << "," << height << "]" <<  std::endl;
+	}
+	// write png file
+	bool ok=writePNG(png_dest,pixels,width,height,lineBuffer);
+	if(ok){
+		std::cout << "snapshot saved bmp file to " << png_dest << ": [" << width << "," << height << "]" <<  std::endl;
+	}else{
+		std::cout << "writePNG failed save to " << png_dest << ": [" << width << "," << height << "]" <<  std::endl;
+	}
+// dump base 64 in stdout
+//	std::string base64=encodeBase64((uint8_t*)pPixels,width*height*4);
+//	std::cout << base64 << std::endl;
+	return true;
+}
 
-	HDC hdcScreen = GetDC(NULL); 
-	if (!hdcScreen) return 1;
-	HDC hdcMem = CreateCompatibleDC(hdcScreen);
-	if (!hdcMem) {ReleaseDC(NULL, hdcScreen);return 1;}
-
+bool writeImage(HDC hdcMem, HDC hdcSrc, const char *dest,int sx,int sy,int width,int height){
 	BITMAPINFO bmi = {0};
 	bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
 	bmi.bmiHeader.biWidth = width;
@@ -36,43 +51,45 @@ int main(int argc, char* argv[]) {
 	bmi.bmiHeader.biCompression = BI_RGB;
 	void *pPixels = NULL;
 	HBITMAP hBitmap = CreateDIBSection(hdcMem, &bmi, DIB_RGB_COLORS, &pPixels, NULL, 0);
-	if (!hBitmap || !pPixels) {
-		DeleteDC(hdcMem);
-		ReleaseDC(NULL, hdcScreen);
-		return 1;
-	}
+	if (!hBitmap || !pPixels) return false;
 	HGDIOBJ hOldBitmap = SelectObject(hdcMem, hBitmap);
-	BitBlt(hdcMem, 0, 0, width, height, hdcScreen, 0, 0, SRCCOPY);
-
-// write bmp file
-	writeBMP(bmp_dest,pPixels,width,height);
-	std::cout << "snapshot saved bmp " << bmp_dest << ": [" << width << "," << height << "]" <<  std::endl;
-
-// write jpg file
-	bool ok1=writeJPG(jpg_dest,pPixels,width,height,72);
-	if(ok1){
-		std::cout << "snapshot saved jpg " << jpg_dest << ": [" << width << "," << height << "]" <<  std::endl;
-	}else{
-		std::cout << "snapshot failed to save jpg " << jpg_dest << ": [" << width << "," << height << "]" <<  std::endl;
-	}
-
-
-	// write png file
-	bool ok=writePNG(dest,pPixels,width,height);
-	if(ok){
-		std::cout << "snapshot saved bmp file to " << dest << ": [" << width << "," << height << "]" <<  std::endl;
-	}else{
-		std::cout << "writePNG failed save to " << dest << ": [" << width << "," << height << "]" <<  std::endl;
-	}
-
-// dump base 64 in stdout
-//	std::string base64=encodeBase64((uint8_t*)pPixels,width*height*4);
-//	std::cout << base64 << std::endl;
-
+	BitBlt(hdcMem, 0, 0, width, height, hdcSrc, sx, sy, SRCCOPY);
+	bool ok=writeImageFile(dest,pPixels,width,height);
 	SelectObject(hdcMem, hOldBitmap);
 	DeleteObject(hBitmap);
-	DeleteDC(hdcMem);
-	ReleaseDC(NULL, hdcScreen);
+	return ok;
+}
+
+int main(int argc, char* argv[]) {
+	SetProcessDPIAware();
+	bool desktop = (argc > 2) && (strcmp(argv[2],"--desktop")==0);
+	const char* dest = (argc > 1) ? argv[1] : "desktop.png";
+	HWND hwnd = GetForegroundWindow();
+	if(!desktop && hwnd){
+		RECT rc;
+		GetWindowRect(hwnd, &rc);
+		int width = rc.right - rc.left;
+		int height = rc.bottom - rc.top;
+		HDC hdcScreen = GetDC(NULL);
+		HDC hdcMem = CreateCompatibleDC(hdcScreen);
+		bool ok = writeImage(hdcMem, hdcScreen, dest, rc.left, rc.top, width, height);
+		ReleaseDC(NULL, hdcScreen);
+		DeleteDC(hdcMem);		
+	}else{
+		int width = GetSystemMetrics(SM_CXSCREEN);
+		int height = GetSystemMetrics(SM_CYSCREEN);
+		HDC hdcScreen = GetDC(NULL); 
+		if (!hdcScreen) return 1;
+		HDC hdcMem = CreateCompatibleDC(hdcScreen);
+		if (!hdcMem) {ReleaseDC(NULL, hdcScreen);return 1;}
+		bool ok=writeImage(hdcMem,hdcScreen,dest,0,0,width,height);
+		DeleteDC(hdcMem);
+		ReleaseDC(NULL, hdcScreen);
+		if(!ok) {
+			std::cout << "writeImage failure" << std::endl;
+			return 1;
+		}
+	}
 
 	return 0;
 }
--- tools/writepng.cpp
+++ tools/writepng.cpp
@@ -3,51 +3,58 @@
 #include <png.h>
 #include <cstdio>
 
-extern "C" bool writePNG(const char* filename, const void* pPixels, int width, int height);
+extern "C" bool writePNG(const char* filename, const void* pPixels, int width, int height, unsigned char *bgrBuffer);
 
 // Custom error handler that does nothing (or you can log here)
 void my_error_fn(png_structp png_ptr, png_const_charp error_msg) {
-    // You could print error_msg to stderr if desired
+	// You could print error_msg to stderr if desired
 }
 
 void my_warning_fn(png_structp png_ptr, png_const_charp warning_msg) {
-    // Ignore warnings
+	// Ignore warnings
 }
 
-bool writePNG(const char* filename, const void* pPixels, int width, int height) {
-    FILE* fp = fopen(filename, "wb");
-    if (!fp) return false;
+bool writePNG(const char* filename, const void* pPixels, int width, int height, unsigned char *bgrBuffer) {
+	FILE* fp = fopen(filename, "wb");
+	if (!fp) return false;
 
-    // Pass custom error functions instead of NULL
-    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, my_error_fn, my_warning_fn);
-    if (!png_ptr) {
-        fclose(fp);
-        return false;
-    }
+	// Pass custom error functions instead of NULL
+	png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, my_error_fn, my_warning_fn);
+	if (!png_ptr) {
+		fclose(fp);
+		return false;
+	}
 
-    png_infop info_ptr = png_create_info_struct(png_ptr);
-    if (!info_ptr) {
-        png_destroy_write_struct(&png_ptr, NULL);
-        fclose(fp);
-        return false;
-    }
+	png_infop info_ptr = png_create_info_struct(png_ptr);
+	if (!info_ptr) {
+		png_destroy_write_struct(&png_ptr, NULL);
+		fclose(fp);
+		return false;
+	}
 
-    png_init_io(png_ptr, fp);
+	png_init_io(png_ptr, fp);
 
-    png_set_IHDR(png_ptr, info_ptr, width, height, 8,
-                 PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE,
-                 PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+	png_set_IHDR(png_ptr, info_ptr, width, height, 8,
+				 PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE,
+				 PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
 
-    png_set_bgr(png_ptr);
-    png_write_info(png_ptr, info_ptr);
+	png_set_bgr(png_ptr);
+	png_write_info(png_ptr, info_ptr);
 
-    for (int y = 0; y < height; y++) {
-        png_write_row(png_ptr, (png_const_bytep)((const unsigned char*)pPixels + (y * width * 4)));
-    }
+	for (int y = 0; y < height; y++) {
+		const unsigned char*p=(png_const_bytep)(const unsigned char*)pPixels + (y * width * 4);            
+		for(int x=0;x<width;x++){
+			bgrBuffer[x*4+0]=p[x*4+2];
+			bgrBuffer[x*4+1]=p[x*4+1];
+			bgrBuffer[x*4+2]=p[x*4+0];
+			bgrBuffer[x*4+3]=255;
+		}
+		png_write_row(png_ptr, bgrBuffer);
+	}
 
-    png_write_end(png_ptr, info_ptr);
-    png_destroy_write_struct(&png_ptr, &info_ptr);
-    fclose(fp);
+	png_write_end(png_ptr, info_ptr);
+	png_destroy_write_struct(&png_ptr, &info_ptr);
+	fclose(fp);
 
-    return true;
+	return true;
 }
