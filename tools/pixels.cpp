65f3edb08a1e7b0c754bd879e7ae537651a55be5
simon ryzen5 <nitrologic@gmail.com>
2026-07-05 16:35:12 +1200

    intPixels 3

--- tools/pixels.cpp
+++ tools/pixels.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include "intpixels.h"
 
 extern "C" bool initFreetype(const char *filepath,int size);
+
+int testImage(const char *src){
+	Path srcPath=src;
+	size_t n = srcPath.string().size();
+	intPixels* image = new intPixels();
+	bool result = image->loadPNG(srcPath.string().c_str());
+	if (!result) {
+		std::cout << "publishRaw loadPNG failure for " << srcPath << std::endl;
+		return -1;
+	}
+	std::cout << "image " << image->width << " x " << image->height << std::endl;
+	return 0;
+}
 
 int main(int argc, char* argv[]) {
    	bool ok=initFreetype("verdanab.ttf",72);
 	std::cout << "initFreetype " << (ok?"OK":"FAIL") << std::endl;
+	int result=testImage("desktop.png");
+	std::cout << "testImage " << (result) << std::endl;
     return 0;
 }
