482ef6af27dc6b110b675dfd403d0d501158183c
simon ryzen5 <nitrologic@gmail.com>
2026-09-11 13:19:01 +1200

    godot runtime 2

---
+++ gd/run.bat
@@ -0,0 +1,4 @@
+@echo off
+rem Godot_v4.7.2-stable_win64_console.exe --help --no-color
+rem Godot_v4.7.2-stable_win64_console.exe --scene control.tscn dot1/project.godot 
+Godot_v4.7.2-stable_win64_console.exe --path "dot1" "res://control.tscn"
